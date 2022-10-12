61d82982cde790bebb0b688de7397fa2a20ab22c
bob <iamvladgubarevofficial@gmail.com>
2022-10-12 20:09:28 +0500

    Сделаны тесты и придуманы решения

---
+++ ReverseClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseLibrary
+{
+    public class ReverseClass
+    {
+        /// <summary>
+        /// Разворот строки
+        /// </summary>
+        /// <param name="text">
+        /// Входная строка
+        /// </param>
+        /// <returns>
+        /// Возвращает результат разворота
+        /// </returns>
+        public string ReverseString(string text)
+        {
+            ////Второй вариант решения
+            //StringBuilder sb = new StringBuilder(text.Length);
+            //for (int i = text.Length; i-- != 0;)
+            //    sb.Append(text[i]);
+            //return sb.ToString();
+
+            text = text.ToLower().Replace(" ", "");
+            return new string(text.Reverse().ToArray());
+        }
+        public static bool IsPalindrome(string textString)
+        {
+           ReverseClass c=new ReverseClass();
+            if (string.IsNullOrEmpty(textString))
+            {
+                return false;
+            }
+            if (c.ReverseString(textString)== textString.ToLower().Replace(" ", ""))
+            {
+                
+                return true;
+            }
+            return false;
+        }
+    }
+}
